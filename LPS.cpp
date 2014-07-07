92bc53018443d6cac7f580a080c050e4ce23b627
Udbhav Sharma <udbhav.sharma15@gmail.com>
2014-07-07 18:23:48 +0530

    LPS added

--- LPS.cpp
+++ LPS.cpp
@@ -3,14 +3,12 @@
 
 using namespace std;
 
+#define LEFT 1
+#define DIAGONAL 2
+#define UP 3
+
 char X[100]=" ",Y[100]=" ";
 int B[100][100],C[100][100],X_len,Y_len;
-
-/*
-For immediate left i have used value 1
-For diagonally upwards i have used value 2
-For upwards i have used value 3
-*/
 
 void LPS()
 {
@@ -23,17 +21,17 @@ void LPS()
             if(X[i]==Y[j])
             {
                 C[i][j]=C[i-1][j-1]+1;
-                B[i][j]=2;
+                B[i][j]=DIAGONAL;
             }
             else if(C[i][j-1]>=C[i-1][j])
             {
                 C[i][j]=C[i][j-1];
-                B[i][j]=1;
+                B[i][j]=LEFT;
             }
             else
             {
                 C[i][j]=C[i-1][j];
-                B[i][j]=3;
+                B[i][j]=UP;
             }
         }
     }
@@ -45,15 +43,15 @@ void PRINT_LPS(int m,int n)
         return ;
     switch (B[m][n])
     {
-        case 1:
-            PRINT_LPS(m,n-1);
+        case LEFT:
+            PRINT_LCS(m,n-1);
             break;
-        case 2:
-            PRINT_LPS(m-1,n-1);
+        case DIAGONAL:
+            PRINT_LCS(m-1,n-1);
             cout<<X[m];
             break;
-        case 3:
-            PRINT_LPS(m-1,n);
+        case UP:
+            PRINT_LCS(m-1,n);
             break;
     }
 }
